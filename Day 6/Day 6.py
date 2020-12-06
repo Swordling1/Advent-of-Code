eb696effb3a5e09423aee6338754a5228578b640
Swordling1 <floridahunter02@gmail.com>
2020-12-06 11:52:35 -0500

    Finished day 6, that was way too hard

--- Day 6/Day 6.py
+++ Day 6/Day 6.py
@@ -6,16 +6,34 @@ raw = raw.split("\n\n")
 
 length = len(raw)
 
-for i in range(length):
-    raw[i] = raw[i].replace("\n", "")
+part_1 = []
+
+for i in range(length): part_1.append(raw[i].replace("\n", ""))
 
 
 count = 0
 
 for x in range(length):
     c = ""
-    for y in range(len(raw[x])):
-        if raw[x][y] not in c: c = c + raw[x][y]
+    for y in range(len(part_1[x])):
+        if part_1[x][y] not in c: c = c + part_1[x][y]
     count += len(c)
 
+print(count)
+
+count = 0
+part_2 = []
+
+for i in range(length): part_2.append(raw[i].split("\n"))
+
+for x in range(length):
+    c = []
+    for y in range(len(part_2[x][0])):
+        if part_2[x][0][y] not in c: c.append(part_2[x][0][y])
+    for y in range(len(part_2[x])):
+        for i in range(len(c)):
+            if c[i] not in part_2[x][y]: 
+                m = True
+                c[i] = " "
+    count += len(c) - c.count(" ")
 print(count)
