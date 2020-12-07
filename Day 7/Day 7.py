f83a4a96160015cceb4856be9d52149ae01af03b
Cotton <171016560@edu.leonschools.net>
2020-12-07 11:16:52 -0500

    Made some progress on day 7 part 1

--- Day 7/Day 7.py
+++ Day 7/Day 7.py
@@ -6,3 +6,17 @@ raw = raw.strip()
 raw = raw.split("\n")
 
 length = len(raw)
+
+for i in range(length):
+    raw[i] = raw[i].replace(".", "")
+    raw[i] = raw[i].split(" contain ")
+    if "no other" in raw[i][1]: raw[i][1] = "none"
+    else: raw[i][1] = raw[i][1].split(", ")
+
+bags = {}
+
+for i in range(length):
+    if raw[i][1] == "none": bags[raw[i][0]] = "none"
+    else: 
+        bags[raw[i][0]] = {}
+print(bb)
