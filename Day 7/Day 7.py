f03146507e1f7f91a57394b74b89276fb907e3b5
Cotton <171016560@edu.leonschools.net>
2020-12-08 10:59:02 -0500

    I may have did it wrong, will continue tonight

--- Day 7/Day 7.py
+++ Day 7/Day 7.py
@@ -22,9 +22,21 @@ for x in range(length):
     bags.append(raw[x][0])
     if type(raw[x][1]) == CLASS: 
         info.append(raw[x][1])
-        for y in range(len(info[x])):
-            num = info[x][y][0]
-            bag = info[x][y][2:]
-
-            info[x][y] = [info[x][y][0], info[x][y][2:]]
+        for y in range(len(info[x])): info[x][y] = [int(info[x][y][0]), info[x][y][2:]]
     else: info.append("none")
+
+needed = ["shiny gold bags"]
+count = 0
+
+while len(needed) > 0:
+    new = []
+    for i in range(len(needed)):
+        need = bags.index(needed[i])
+        if type(info[need]) == CLASS:
+            for x in range(len(info[need])):
+                for y in range(info[need][x][0]): 
+                    new.append(info[need][x][1])
+                    count +=1
+        else: count +=1
+    needed = new
+print(count)
