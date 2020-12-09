2ec3738c5d1c64a5dc31a518ee4a2b0693f72039
Cotton <171016560@edu.leonschools.net>
2020-12-09 11:30:02 -0500

    AAAAAAAAAAAAA

--- Day 8/Day 8.py
+++ Day 8/Day 8.py
@@ -14,9 +14,39 @@ for i in range(length):
 
 acc = 0
 i = 0
+flag = []
 
 while True:
     if raw[i][2]: break
+    if raw[i][0] == "nop": 
+        raw[i][2] = True
+        if raw[i][1] != 0: flag.append(i)
+        i += 1
+    elif raw[i][0] == "acc":
+        acc += raw[i][1]
+        raw[i][2] = True
+        i += 1
+    elif raw[i][0] == "jmp":
+        raw[i][2] = True
+        i += raw[i][1]
+
+print(acc)
+
+latest = 0
+for i in range(length):
+    if i > latest and raw[i][2]: latest = i
+
+for i in range(length):
+    raw[i][2] = False
+
+
+acc = 0
+i = 0
+
+while i < length:
+    if raw[i][2]: 
+        print('ERROR IN LOOP!!!')
+        break
     if raw[i][0] == "nop": 
         raw[i][2] = True
         i += 1
