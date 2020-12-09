3e7829ec44f87396ecf396af9fa68a7232ca7241
Swordling1 <floridahunter02@gmail.com>
2020-12-09 18:13:41 -0500

    Finished part 1 of day 9

--- Day 9/Day 9.py
+++ Day 9/Day 9.py
@@ -1,9 +1,25 @@
 from pathlib import Path
 import copy
 
-raw = Path('Day 8\input.txt').read_text()
-#raw = Path('Day 8\\test.txt').read_text()
+raw = Path('Day 9\input.txt').read_text()
+#raw = Path('Day 9\\test.txt').read_text()
 raw = raw.strip()
 raw = raw.split("\n")
 
 length = len(raw)
+start = 25
+
+list = []
+for i in range(length): list.append(int(raw[i]))
+
+invalid = 0
+for i in range(start, length):
+    o = i + 1
+    num = list[i - start:i]
+    valid = False
+    for x in range(start):
+        for y in range(start):
+            if num[x] + num[y] == list[i]: valid = True
+    if valid == False: invalid = list[i]
+
+print(invalid)
