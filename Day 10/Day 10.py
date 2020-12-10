c3b9534f3ef37f7c38bb80c042f1cc167594861a
Cotton <171016560@edu.leonschools.net>
2020-12-10 10:37:11 -0500

    Finished day 10 part 1

--- Day 10/Day 10.py
+++ Day 10/Day 10.py
@@ -10,4 +10,22 @@ length = len(raw)
 
 list = []
 for i in range(length): list.append(int(raw[i]))
+list.sort()
 
+num = 0
+jolt1 = 0
+jolt3 = 0
+
+for i in range(length):
+    if list[i] == num + 3:
+        jolt3 += 1
+        num = list[i]
+    if list[i] == num + 1:
+        jolt1 += 1
+        num = list[i]
+jolt3 += 1
+
+print(num + 3)
+print(jolt1)
+print(jolt3)
+print(jolt1 * jolt3)
